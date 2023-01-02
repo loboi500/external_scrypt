8deab9d9b2c16d79575f829a9a1175ed6000a79a
Trilokesh Rangam <rangam.lokesh@gmail.com>
2023-01-02 08:01:13 +0530

    lfs tracking enabled
    Signed-off-by: Trilokesh Rangam <rangam.lokesh@gmail.com>

--- scrypt_platform.h
+++ scrypt_platform.h
@@ -1,12 +1,3 @@
-#ifndef _SCRYPT_PLATFORM_H_
-#define	_SCRYPT_PLATFORM_H_
-
-#if defined(CONFIG_H_FILE)
-#include CONFIG_H_FILE
-#elif defined(HAVE_CONFIG_H)
-#include "config.h"
-#else
-#error Need either CONFIG_H_FILE or HAVE_CONFIG_H defined.
-#endif
-
-#endif /* !_SCRYPT_PLATFORM_H_ */
+version https://git-lfs.github.com/spec/v1
+oid sha256:da0fee78e744f01d8d0e2d09067363f9c918b59aa58b41fcb9feb01ad40a5eaa
+size 263
